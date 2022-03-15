fd51872e78261032514d8eab7e2020c89d6b96a6
Skyblueborb <tomaszborbely0710@gmail.com>
2022-03-15 18:26:26 +0100

    something works

--- main.c
+++ main.c
@@ -5,13 +5,27 @@
 
 int main()
 {
-    char ch, file_name[25];
+    char ch;
     FILE *fp;
+    char txt[127];
+    int iter = 0;
+    int fcontent;
     static __uintmax_t rxbytes;
-    char path[PATH_MAX];
     fp = fopen("/sys/class/net/enp4s0/statistics/rx_bytes", "r");
-    while((ch = fgetc(fp)) != EOF)
-      printf("%c", ch);
-
+   //  long size = fsize(fp);
+   //  fcontent = malloc(size);
+   //  fread(fcontent, 1, size, fp);
+   while((ch = getc(fp)) != EOF) {
+    txt[iter] = putchar(ch);
+    iter++;
+}
+   //  while((ch = fgetc(fp)) != EOF)
+      // txt[iter] = ch;
+      // printf("%c", ch);
+      // iter++;
+   // txt[0] = fgetc(fp);
    fclose(fp);
+   printf(txt);
+   // rxbytes = atoll(txt);
+   // printf("%llu", rxbytes);
 }
