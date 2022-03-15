6ee8fe0d074af1d92cb1518b61f4ceba5f777aea
Skyblueborb <tomaszborbely0710@gmail.com>
2022-03-15 20:03:02 +0100

    somewhat works tbh

--- main.c
+++ main.c
@@ -1,31 +1,37 @@
 #include <stdio.h>
-#include <limits.h>
-#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 
+#define BASE 10
+
+void readBytes(FILE *fp, __uintmax_t* out ) {
+   char *ptr;
+   char buffer[128];
+   if (fp == NULL) {
+      perror("Unable to open file");
+      exit(EXIT_FAILURE);
+   }
+   fread(buffer, 128, 1, fp);
+   fclose(fp);
+   *out = strtoul(buffer, &ptr, BASE);
+   memset(buffer, 0, 128);
+}
 
 int main()
 {
-    char ch;
-    FILE *fp;
-    char txt[127];
-    int iter = 0;
-    int fcontent;
-    static __uintmax_t rxbytes;
-    fp = fopen("/sys/class/net/enp4s0/statistics/rx_bytes", "r");
-   //  long size = fsize(fp);
-   //  fcontent = malloc(size);
-   //  fread(fcontent, 1, size, fp);
-   while((ch = getc(fp)) != EOF) {
-    txt[iter] = putchar(ch);
-    iter++;
-}
-   //  while((ch = fgetc(fp)) != EOF)
-      // txt[iter] = ch;
-      // printf("%c", ch);
-      // iter++;
-   // txt[0] = fgetc(fp);
-   fclose(fp);
-   printf(txt);
-   // rxbytes = atoll(txt);
-   // printf("%llu", rxbytes);
+   FILE *fprx = fopen("/sys/class/net/enp4s0/statistics/rx_bytes", "r");
+   FILE *fptx = fopen("/sys/class/net/enp4s0/statistics/tx_bytes", "r");
+   static __uintmax_t rxbytes;
+   static __uintmax_t txbytes;
+  
+   readBytes(fprx, &rxbytes);
+   readBytes(fptx, &txbytes);
+   
+   printf("Data downloaded: %lluMB\n",rxbytes/1000000);
+   printf("Data uploaded: %lluMB\n",txbytes/1000000);
+   printf("Raw bytes downloaded: %llu\n",rxbytes);
+   printf("Raw bytes uploaded: %llu\n",txbytes);
+
+
+   return 0;
 }
