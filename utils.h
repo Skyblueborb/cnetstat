4aa28a06912a11663a643b606f6f201e027f2e77
Fishhh <hubert.gluchowski19@gmail.com>
2022-06-12 17:06:02 +0200

    Fix and clean up `find_adapter`

--- utils.h
+++ utils.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <sys/types.h>
+#include <stdio.h>
 #include <time.h>
 
 #define eprintf(fmt, ...) fprintf(stderr, fmt, ##__VA_ARGS__)
@@ -7,12 +8,29 @@
 char *get_user_name();
 
 /**
- * Just like mkdir(2) but recursively creates parent directories.
+ * Like mkdir(2) but recursively creates parent directories.
  **/
 int mkdirp(char *path, mode_t mode);
 
 /**
-  * Finds the adapter name to use.
+ * Like perror(3) but accepts a format string.
+ */
+void perrorf(const char *fmt, ...);
+
+/**
+ * Reads one word from a file.
+ * A word is defined as a sequence of non-whitespace characters.
+ * Whitespace is defined as characters for which isspace(3) returns true.
+ *
+ * @param file The file to read from.
+ * @param wordptr A pointer to a char* that will be set to the word (has to be freed with free()).
+ * @return Word length on success, -1 on error.
+ */
+ssize_t getword(FILE* file, char** wordptr);
+
+/**
+  * Returns the name of the first active interface.
+  * Ignores loopback and virtual interfaces.
   **/
 char* find_adapter();
 
